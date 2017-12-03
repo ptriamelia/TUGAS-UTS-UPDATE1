9ab728ac5b026317cf9132f2e2e85afe8d815e20
Divian Ajie Permana <32475802+DivianPermana@users.noreply.github.com>
2017-12-03 14:58:01 +0700

    Add files via upload

---
+++ TugasUpdateUTS/src/Model/DataFitness.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author MY PC
+ */
+public class DataFitness {
+
+    private String noKtp, nama, jeniskelamin, cabang, handuk, paketfitness;
+    private int biayapendaftaran;
+
+    public String getNoKtp() {
+        return noKtp;
+    }
+
+    public void setNoKtp(String noKtp) {
+        this.noKtp = noKtp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJeniskelamin() {
+        return jeniskelamin;
+    }
+
+    public void setJeniskelamin(String jeniskelamin) {
+        this.jeniskelamin = jeniskelamin;
+    }
+
+    public String getCabang() {
+        return cabang;
+    }
+
+    public void setCabang(String cabang) {
+        this.cabang = cabang;
+    }
+
+    public String getHanduk() {
+        return handuk;
+    }
+
+    public void setHanduk(String handuk) {
+        this.handuk = handuk;
+    }
+
+    public String getPaketfitness() {
+        return paketfitness;
+    }
+
+    public void setPaketfitness(String paketfitness) {
+        this.paketfitness = paketfitness;
+    }
+
+    public int getBiayapendaftaran() {
+        return biayapendaftaran;
+    }
+
+    public void setBiayapendaftaran(int biayapendaftaran) {
+        this.biayapendaftaran = biayapendaftaran;
+    }
+
+}
